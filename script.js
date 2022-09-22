9231843b903011fd79ca2081ac6049c06d3d1480
mars <abderrazzaqmars99@gmail.com>
2022-09-22 14:26:39 +0100

    1er possibilité

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 // Déclaration
 
-let turn = 'x';
+let tour = 'x';
+let cadr = [];
+
+
+
+// // Function Win
+
+function win() {
+
+    for (let i = 1; i < 10; i++) {
+        cadr[i] = document.getElementById('c' + i).innerHTML;
+    }
+    console.log(cadr)
+
+    if (cadr[1] == cadr[2] && cadr[2] == cadr[3] && cadr[1] != '') {
+        document.getElementById('c1').style.background = '#000'
+        document.getElementById('c2').style.background = '#000';
+        document.getElementById('c3').style.background = '#000';
+    }
+}
+
+// Function Game
+
+function game(id) {
+    let element = document.getElementById(id);
+    if (tour == 'x' && element.innerHTML == '') {
+        document.getElementById('px').style.fontSize = "bold";
+        element.innerHTML = 'x';
+        tour = 'o';
+    } else if (tour == 'o' && element.innerHTML == '') {
+        document.getElementById('po').style.fontSize = "bold";
+        element.innerHTML = 'o';
+        tour = 'x';
+    }
+    win();
+}
 
 // Function Reset
 
@@ -14,19 +49,6 @@ function Reset() {
     let cases = document.querySelectorAll(".case")
     cases.forEach(element => {
         element.innerHTML = ""
-        turn = 'x';
+        tour = 'x';
     });
-}
-
-// Function Game
-
-function game(id) {
-    let element = document.getElementById(id);
-    if (turn === 'x' && element.innerHTML == '') {
-        element.innerHTML = 'x';
-        turn = 'o';
-    } else if (turn === 'o' && element.innerHTML == '') {
-        element.innerHTML = 'o';
-        turn = 'x';
-    }
 }
